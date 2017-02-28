82539f33340f9728fe70e9f9dd404af157e13522
Ben Warren <ben.warren@cambia.org>
2017-02-28 04:46:22 +0000

    use bash rather than sh

--- data/parse.sh
+++ data/parse.sh
@@ -1,11 +1,11 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 set -e
 
 script_dir=$(cd "$(dirname "$0")"; pwd)
 cd "$script_dir"
 
-parser="../pat-clas-parse/target/pat-clas-parse-*.one-jar.jar"
+parser='../pat-clas-parse/target/pat-clas-parse-*.one-jar.jar'
 
 if ! ls $parser &> /dev/null ; then
   echo "Parser jar not found: $parser" ; exit 1
